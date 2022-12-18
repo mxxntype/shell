cd543ad05d9d169031e8678b381792632c0b1839
astrumaureus <astrumaureus@warden>
2022-12-18 22:30:42 +0300

    wallpaper: add #1e1e2e background

--- wallpaper.sh
+++ wallpaper.sh
@@ -3,7 +3,7 @@
 THRESHOLD=0
 SWWW_DAEMONS=$(ps -e | grep -c 'swww')
 if [ $SWWW_DAEMONS -le $THRESHOLD ]; then
-  swww init
+  swww init && swww clear 1e1e2e
 fi
 
 sleep 0.2 && swww img ~/.config/wallpapers/sunset.gif
