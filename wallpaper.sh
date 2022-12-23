915b33372be35f1a71aca4a53b28b5db0cf318d1
astrumaureus <astrumaureus@warden>
2022-12-23 19:21:46 +0300

    add multiple modes with tofi prompts

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,15 +1,60 @@
 #!/bin/sh
 
-THRESHOLD=0
-SWWW_DAEMONS=$(ps -e | grep -c 'swww')
-if [ $SWWW_DAEMONS -le $THRESHOLD ]; then
-  swww init && swww clear 1e1e2e
-fi
-
+# general
 WALLPAPER_DIR=$HOME/.config/wallpapers
 WALLPAPER_STYLE=gradient
 
-WALLPAPER_FILE="$(ls $WALLPAPER_DIR/$WALLPAPER_STYLE | sort -R | head -n 1)"
-echo $WALLPAPER_FILE
+# start swww daemon if none already running
+init () {
+  SWWW_DAEMONS=$(ps -e | grep -c 'swww')
+  if [ $SWWW_DAEMONS -le "0" ]; then
+    swww init && swww clear 1e1e2e && echo "init: successful"
+  fi
+}
 
-sleep 0.2 && swww img --outputs=HDMI-A-1 $WALLPAPER_DIR/$WALLPAPER_STYLE/$WALLPAPER_FILE
+# picks a random wallpaper
+random () {
+  WALLPAPER_FILE="$(ls $WALLPAPER_DIR/$WALLPAPER_STYLE | sort -R | head -n 1)"
+}
+
+# ask the user for a wallpaper through tofi
+interactive () {
+  TEXT_RANDOM=" Random $WALLPAPER_STYLE"
+  TEXT_DARK=" Dark $WALLPAPER_STYLE"
+  TEXT_MANUAL="Select manually..."
+  CHOICE=$( echo -e "$TEXT_RANDOM\n$TEXT_DARK\n$TEXT_MANUAL" |
+    tofi  --prompt-text="" --placeholder-text="Pick an option:" --placeholder-color="cdd6f4" --width=200 --height=112 )
+  if [[ "$CHOICE" = "$TEXT_RANDOM" ]]; then
+    random
+  elif [[ "$CHOICE" = "$TEXT_DARK" ]]; then
+    WALLPAPER_FILE="base-srf0.jpg"
+  elif [[ "$CHOICE" = "$TEXT_MANUAL" ]]; then
+    WALLPAPER_FILE=$(ls $WALLPAPER_DIR/$WALLPAPER_STYLE |
+      tofi --prompt-text="" --placeholder-text="Pick a wallpaper:" --placeholder-color="cdd6f4" --width=240 )
+  fi
+}
+
+# sets the wallpaper
+apply () {
+  if [ -f "$WALLPAPER_DIR/$WALLPAPER_STYLE/$WALLPAPER_FILE" ]; then
+    sleep 0.2 && swww img --outputs=HDMI-A-1 $WALLPAPER_DIR/$WALLPAPER_STYLE/$WALLPAPER_FILE &&
+      echo "applied: $WALLPAPER_STYLE/$WALLPAPER_FILE."
+    export WALLPAPER_FILE
+  else
+    echo "$WALLPAPER_DIR/$WALLPAPER_STYLE/$WALLPAPER_FILE: file does not exist."
+  fi
+  export WALLPAPER_DIR
+  export WALLPAPER_STYLE
+}
+
+init
+
+if [[ "$1" = "--random" ]] || [[ "$1" = "-r" ]]; then
+  echo "running in random mode."
+  random && apply
+elif [[ "$1" = "--interactive" ]] || [[ "$1" = "-i" ]]; then
+  echo "running in interactive mode."
+  interactive && apply
+else
+  echo "error: no/invalid mode supplied. exiting..."
+fi
