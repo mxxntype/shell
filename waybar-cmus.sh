8a21e241d8b5982a8f9a1021c3a0473948864945
astrumaureus <astrumaureus@warden>
2022-12-24 14:35:17 +0300

    upload

---
+++ waybar-cmus.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+CMUS_FILE=$( cmus-remote -C "format_print %f" )
+CMUS_TRACKNAME=$( echo $CMUS_FILE | sed -e "s#.*/##" | sed -e "s/.mp3//" | sed -e "s/.* - //" )
+CMUS_OUTPUT=$CMUS_TRACKNAME
+
+CMUS_STATUS="$( echo -e $( cmus-remote -C status ) )"
+
+if [ "$( echo $CMUS_STATUS | grep -c 'status playing' )" -gt "0" ]; then
+  if [ "$( echo $CMUS_STATUS | grep -c 'set repeat_current true' )" -gt "0" ]; then
+    CMUS_OUTPUT="$CMUS_OUTPUT "
+  fi
+  echo "$CMUS_OUTPUT"
+fi
