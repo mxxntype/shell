5619d58db58320b19cef43392a298f376ba85f1c
astrumaureus <astrumaureus@warden>
2022-12-24 15:03:05 +0300

    format & fix path

--- opaque-probe.sh
+++ opaque-probe.sh
@@ -2,8 +2,8 @@
 
 THRESHOLD=2
 
-if [ -x "$(command -v opaque)" ]; then
-  OPEN_TERMINALS=$(ps -e | grep -c 'zsh')
+if [ -x "$( command -v opaque.sh )" ]; then
+  OPEN_TERMINALS=$( ps -e | grep -c 'zsh' )
   if [ $OPEN_TERMINALS -le $THRESHOLD ]; then
     exec opaque.sh
   fi
