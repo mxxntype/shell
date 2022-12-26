5db359d7b609d58254530ca3076af7a783d5cbdc
astrumaureus <astrumaureus@warden>
2022-12-26 21:18:52 +0300

    check for + inside []

--- waybar-cmus.sh
+++ waybar-cmus.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 CMUS_FILE=$( cmus-remote -C "format_print %f" )
-CMUS_TRACKNAME=$( echo $CMUS_FILE | sed -e "s#.*/##" | sed -e "s/.mp3//" | sed -e "s/.* - //" | sed -e "s/ \[.*\]//" )
+CMUS_TRACKNAME=$( echo $CMUS_FILE | sed -e "s#.*/##" | sed -e "s/.mp3//" | sed -e "s/.* - //" | sed -e "s/ \[\+.*\]//" )
 CMUS_OUTPUT=$CMUS_TRACKNAME
 
 CMUS_STATUS="$( echo -e $( cmus-remote -C status ) )"
