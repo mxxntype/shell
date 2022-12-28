3010512cb8deb2d81ac1753257675b4f672ce669
astrumaureus <astrumaureus@warden>
2022-12-28 22:28:28 +0300

    Revert "change waybar reload method"
    This reverts commit 21e3fe81a8c6eb32f5fb13c352c3a267c0c72155.

--- waybar-start.sh
+++ waybar-start.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-if [ "$(ps -e | grep -c waybar)" -eq "0" ]; then
-  waybar & # start waybar if not running already
-else
-  killall -SIGUSR2 waybar && # reload waybar & send notification
-    notify-send "Waybar reloaded." -i /usr/share/icons/ePapirus-Dark/32x32/apps/systemtray.svg
+# kill running waybar instances
+if [ "$(ps -e | grep -c waybar)" -gt "0" ]; then
+  killall -9 waybar
 fi
+
+# start waybar & send a notification
+waybar &
+notify-send "Waybar reloaded." -i /usr/share/icons/ePapirus-Dark/32x32/apps/systemtray.svg
 
 # waybar doesn't seem to show the keyboard language until it is changed
 # for the first time, this does nothing noticeable but fixes the issue
