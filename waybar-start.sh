dc7bc631ad73c4ea67e45ed17062a024c8fdbefe
astrumaureus <astrumaureus@warden>
2022-12-29 16:36:51 +0300

    increase kb fix delay

--- waybar-start.sh
+++ waybar-start.sh
@@ -11,6 +11,6 @@ notify-send "Waybar reloaded." -i /usr/share/icons/ePapirus-Dark/32x32/apps/syst
 
 # waybar doesn't seem to show the keyboard language until it is changed
 # for the first time, this does nothing noticeable but fixes the issue
-sleep 0.1 &&
+sleep 0.25 &&
 hyprctl switchxkblayout cooler-master-technology-inc.-gaming-mech-kb 1
 hyprctl switchxkblayout cooler-master-technology-inc.-gaming-mech-kb 0
